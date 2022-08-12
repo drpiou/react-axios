9070025fd3f12ed43265ebff43edf0f403e688c6
Alex <dr.piou@gmail.com>
2022-08-12 02:34:00 +0200

    using vite

--- vite.config.ts
+++ vite.config.ts
@@ -19,12 +19,12 @@ export default defineConfig({
     },
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled into your library
-      external: ['react', '@drpiou/axios'],
+      external: ['@drpiou/axios', 'react'],
       output: {
         // Provide global variables to use in the UMD build for externalized deps
         globals: {
-          react: 'React',
           '@drpiou/axios': 'DrAxios',
+          react: 'React',
         },
       },
     },
